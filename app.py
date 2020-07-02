d7e58ec8d309d8cee117da549b135b66154d0f51
AnnaDK <ankrutikova@gmail.com>
2020-07-02 11:41:00 +0000

    comments added in app.py

--- app.py
+++ app.py
@@ -17,12 +17,15 @@ mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/index')
+# Home page shows 'welcome' text
 def index():
     return render_template("index.html", title="Home", recipes=mongo.db.recipes.find())
 
 
 @app.route('/get_recipes')
+# Displaying recipes collection with a choice of categories 
 def get_recipes():
+    # Logic for pagination
     per_page = 8
     page = int(request.args.get('page', 1))
     total = mongo.db.recipes.count_documents({})
@@ -32,12 +35,15 @@ def get_recipes():
 
 
 @app.route('/add_recipes')
+# User can create a recipe and it will be added into recipe collection
 def add_recipes():
     return render_template("add_recipe.html", title="Add Recipe", categories=mongo.db.categories.find())
 
 
 @app.route('/fruit_smoothies')
+# Route to the category of fruit smoothies
 def fruit_smoothies():
+    # Logic for pagination
     per_page = 8
     page = int(request.args.get('page', 1))
     total = mongo.db.recipes.count_documents({})
@@ -47,7 +53,9 @@ def fruit_smoothies():
 
 
 @app.route('/green_smoothies')
+# Route to the category of green smoothies
 def green_smoothies():
+    # Logic for pagination
     per_page = 8
     page = int(request.args.get('page', 1))
     total = mongo.db.recipes.count_documents({})
@@ -57,7 +65,9 @@ def green_smoothies():
 
 
 @app.route('/protein_smoothies')
+# Route to the category of protein smoothies
 def protein_smoothies():
+    # Logic for pagination
     per_page = 8
     page = int(request.args.get('page', 1))
     total = mongo.db.recipes.count_documents({})
@@ -67,24 +77,23 @@ def protein_smoothies():
 
 
 @app.route('/blenders')
+# Displaying blenders collection with a link to the shop 
 def blenders():
     return render_template("blenders.html", title="Blenders", blenders=mongo.db.blenders.find())
 
 
 @app.route('/insert_recipe', methods=['POST'])
+# The route to the form for inserting created recipe
 def insert_recipe():
-    """Creating a recipe and adding it into recipe collection"""
-    
     if request.method == "POST":
         recipes = mongo.db.recipes
         recipes.insert_one(request.form.to_dict())
-    
         flash("Thank you for adding a recipe!")
-        
         return render_template("add_recipe.html", categories=mongo.db.categories.find())
 
 
 @app.route('/edit_recipe/<recipe_id>')
+# The route to the form for editing existing recipe
 def edit_recipe(recipe_id):
     the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     all_categories = mongo.db.categories.find()
@@ -92,6 +101,7 @@ def edit_recipe(recipe_id):
 
 
 @app.route('/update_recipe/<recipe_id>', methods=['POST'])
+# The route to the form for inserting updated recipe
 def update_recipe(recipe_id):
     recipes = mongo.db.recipes
     recipes.update({'_id': ObjectId(recipe_id)}, {   
@@ -106,10 +116,11 @@ def update_recipe(recipe_id):
 
 
 @app.route('/delete_recipe/<recipe_id>', methods=['POST'])
+# The route to delete the existing recipe. The recipe will be deleted from the database
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
     return redirect(url_for('get_recipes'))
-    
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
